386409b90929e09dbdff714216ff51feef8d1f3e
Renan Souza <renantrsouza@gmail.com>
2021-06-23 13:14:10 -0400

    criei e adcionei um item em um array

---
+++ listas.js
@@ -0,0 +1,6 @@
+console.log('Trabalhando com listas');
+var estados = ['Amazonas', 'Pará', 'São Paulo'];
+console.log(estados);
+
+estados.push('Rio de Janeiro'); //método push serve para incluir um item alista, semelhante ao .append() do pyton
+console.log(estados);
