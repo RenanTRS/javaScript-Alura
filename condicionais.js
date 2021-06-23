b8d7282d7aed5ddc64ddc2c81fe1d4524ae9dc19
Renan Souza <renantrsouza@gmail.com>
2021-06-23 13:36:19 -0400

    initial commit

---
+++ condicionais.js
@@ -0,0 +1,12 @@
+function mostra(msg){
+    console.log(msg);
+}
+
+mostra('Trabalhando com condicionais');
+var idade = 18;
+
+if(idade < 18){
+    mostra('Menor de idade');
+}else{
+    mostra('Maior de idade')
+}
