9988f3294eeaf9964b0c1f37922c42fb9f168d3d
Renan Souza <renantrsouza@gmail.com>
2021-06-23 13:25:10 -0400

    excluí um item da lista

--- listas.js
+++ listas.js
@@ -4,3 +4,6 @@ console.log(estados);
 
 estados.push('Rio de Janeiro'); //método push serve para incluir um item alista, semelhante ao .append() do pyton
 console.log(estados);
+
+estados.splice(1,1); //Se não colocar a vírgula e a quantidade ele irá limpar o array
+console.log(estados);
