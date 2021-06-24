2dea0a5619ff4bb5264bff656a40db7e5e20b00a
Renan Souza <renantrsouza@gmail.com>
2021-06-24 17:06:20 -0400

    add file debbug lauch and file loops

--- loops.js
+++ loops.js
@@ -3,18 +3,21 @@ function mostra(msg){
 }
 
 var destinos = ['Amazonas', 'São Paulo', 'Ceará', 'Rio de Janeiro'];
-var destino = 'Rio de Janeiro';
+var destino = 'Ceará';
 var existe = false;
 var cont = 0;
 
-while(cont < 4){
+while(cont < destinos.length){
     if(destinos[cont] == destino){
         mostra('Destino existe')
         existe = true;
+        destinos.splice(cont,1);
         break
     }
     cont += 1;
 }
+
 if(existe == false){
     mostra('Destino inexistente');
 }
+mostra(destinos);
